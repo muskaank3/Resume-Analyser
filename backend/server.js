2725f200893099df2bb9c04b819fd50860df621a
Muskan Khan <khanmuskaaan001@gmail.com>
2026-04-27 17:26:53 +0530

    fixed api route

--- backend/server.js
+++ backend/server.js
@@ -251,6 +251,8 @@ app.post("/upload", upload.single("resume"), async (req, res) => {
 });
 
 // ================= SERVER START =================
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
